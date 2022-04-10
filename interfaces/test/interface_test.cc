b39972fa3c264daacff11302172b8169ed13200d
Frank <frank.permenter@tri.global>
2022-04-10 17:55:19 -0400

    Improve file names.

--- interfaces/test/interface_test.cc
+++ interfaces/test/interface_test.cc
@@ -2,120 +2,87 @@
 
 #include "gtest/gtest.h"
 
-TEST(TestArguments, AddLMI) {
+#define TEST_CONSTRAINT_API(AddConstraintCommand)                              \
+  int status;                                                                  \
+  void* p = CONEX_CreateConeProgram();                                         \
+  status = CONEX_SetNumberOfVariables(p, num_vars);                            \
+  status = AddConstraintCommand(p, order, &constraint_id);                     \
+  EXPECT_EQ(CONEX_SUCCESS, status);                                            \
+                                                                               \
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, valid_row,         \
+                                      valid_col, 0, 0);                        \
+  EXPECT_EQ(CONEX_SUCCESS, status);                                            \
+                                                                               \
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, valid_row,         \
+                                      valid_col, 0, bad_hyper_complex_dim);    \
+  EXPECT_EQ(CONEX_FAILURE, status);                                            \
+  status =                                                                     \
+      CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, bad_variable, 0, 0); \
+  EXPECT_EQ(CONEX_FAILURE, status);                                            \
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, valid_row,         \
+                                      valid_col, bad_column_index, 0);         \
+  EXPECT_EQ(CONEX_FAILURE, status);                                            \
+                                                                               \
+  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, 0, 0, 0);              \
+  EXPECT_EQ(CONEX_SUCCESS, status);                                            \
+  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, valid_row, 0, 0);      \
+  EXPECT_EQ(CONEX_SUCCESS, status);                                            \
+                                                                               \
+  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, valid_row, 0,          \
+                                  bad_hyper_complex_dim);                      \
+  EXPECT_EQ(CONEX_FAILURE, status);                                            \
+  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, valid_row,             \
+                                  bad_column_index, 0);                        \
+  EXPECT_EQ(CONEX_FAILURE, status);                                            \
+                                                                               \
+  CONEX_DeleteConeProgram(p);
+
+TEST(TestSOCPInterface, AddConstraint) {
   void* p = CONEX_CreateConeProgram();
   int constraint_id = 0;
-  EXPECT_TRUE(CONEX_NewLinearMatrixInequality(p, 2, 2, &constraint_id) ==
+  EXPECT_TRUE(CONEX_NewLorentzConeConstraint(p, 2, &constraint_id) ==
               CONEX_SUCCESS);
   EXPECT_EQ(0, constraint_id);
-  EXPECT_TRUE(CONEX_NewLinearMatrixInequality(p, 2, 4, &constraint_id) ==
+  EXPECT_TRUE(CONEX_NewLorentzConeConstraint(p, 2, &constraint_id) ==
               CONEX_SUCCESS);
   EXPECT_EQ(1, constraint_id);
 
   void* null_ptr = NULL;
-  EXPECT_TRUE(CONEX_NewLinearMatrixInequality(null_ptr, 2, 2, &constraint_id) ==
+  EXPECT_TRUE(CONEX_NewLorentzConeConstraint(null_ptr, 2, &constraint_id) ==
               CONEX_FAILURE);
 
-  // TODO(FrankPermenter): Uncomment this test if it can be made more reliable.
-  // Currently, corrupted_ptr test is too weak and may not fail.
-  // void* corrupted_ptr = static_cast<double*>(p) + 1;
-  // EXPECT_TRUE(CONEX_NewLinearMatrixInequality(corrupted_ptr, 2,
-  // 2,&constraint_id) == CONEX_FAILURE);
-
-  int bad_complex_dim = 3;
-  EXPECT_TRUE(CONEX_NewLinearMatrixInequality(p, 2, bad_complex_dim,
-                                              &constraint_id) == CONEX_FAILURE);
-
   int bad_order = 0;
-  EXPECT_TRUE(CONEX_NewLinearMatrixInequality(p, bad_order, 2,
-                                              &constraint_id) == CONEX_FAILURE);
+  EXPECT_TRUE(CONEX_NewLorentzConeConstraint(p, bad_order, &constraint_id) ==
+              CONEX_FAILURE);
   CONEX_DeleteConeProgram(p);
 }
 
-TEST(TestArguments, UpdateLMI) {
-  void* p = CONEX_CreateConeProgram();
+TEST(TestSOCPInterface, UpdateConstraint) {
   int num_vars = 4;
-  int status = CONEX_SetNumberOfVariables(p, num_vars);
-  EXPECT_EQ(CONEX_SUCCESS, status);
+  int constraint_id = 0;
+  int order = 2;
+
+  int bad_hyper_complex_dim = 1;
+  int bad_variable = -1;
+  int bad_column_index = 1;
+  int valid_row = 2;
+  int valid_col = order - 1;
+
+  TEST_CONSTRAINT_API(CONEX_NewLorentzConeConstraint);
+}
+
+TEST(TestLPInterface, UpdateConstraint) {
+  int num_vars = 2;
 
   int constraint_id = 0;
-  int constraint_id_2 = 0;
-  int order = 2;
-  int hyper_complex_dim = 2;
+  int order = 2; /* num rows*/
 
-  status = CONEX_NewLinearMatrixInequality(p, order, hyper_complex_dim,
-                                           &constraint_id);
-  EXPECT_EQ(CONEX_SUCCESS, status);
+  int bad_hyper_complex_dim = 1;
+  int bad_variable = -1;
+  int bad_column_index = num_vars + 2;
 
-  status = CONEX_NewLinearMatrixInequality(p, order, hyper_complex_dim,
-                                           &constraint_id_2);
-  EXPECT_EQ(CONEX_SUCCESS, status);
+  int valid_row = 1;
+  int valid_col = num_vars - 1;
 
-  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, order - 1,
-                                      order - 2, hyper_complex_dim - 1);
-  EXPECT_EQ(CONEX_SUCCESS, status);
-
-  int bad_hyper_complex_dim = hyper_complex_dim;
-  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, order - 1,
-                                      order - 2, bad_hyper_complex_dim);
-  EXPECT_EQ(CONEX_FAILURE, status);
-
-  int bad_order = order;
-  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, bad_order,
-                                      order - 2, hyper_complex_dim - 1);
-  EXPECT_EQ(CONEX_FAILURE, status);
-
-  int bad_index = order;
-  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, order - 1,
-                                      bad_index, hyper_complex_dim - 1);
-  EXPECT_EQ(CONEX_FAILURE, status);
-
-  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, order - 1, order - 2,
-                                  bad_hyper_complex_dim);
-  EXPECT_EQ(CONEX_FAILURE, status);
-
-  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, order - 1, order - 2,
-                                  hyper_complex_dim - 1);
-  EXPECT_EQ(CONEX_SUCCESS, status);
-
-  // Updating diagonal part of skew-symmetric matrix should fail.
-  status =
-      CONEX_UpdateAffineTerm(p, constraint_id, .3, 0, 0, hyper_complex_dim - 1);
-  EXPECT_EQ(CONEX_FAILURE, status);
-  CONEX_DeleteConeProgram(p);
-}
-
-TEST(TestArguments, SetVariables) {
-  int num_vars = 4;
-  void* p = CONEX_CreateConeProgram();
-
-  int status = CONEX_SetNumberOfVariables(p, num_vars);
-  EXPECT_EQ(CONEX_SUCCESS, status);
-
-  status = CONEX_SetNumberOfVariables(p, num_vars);
-  EXPECT_EQ(CONEX_FAILURE, status);
-  CONEX_DeleteConeProgram(p);
-}
-
-TEST(TestArguments, UpdateQuadraticCost) {
-  int num_vars = 4;
-  void* p = CONEX_CreateConeProgram();
-  int status = CONEX_SetNumberOfVariables(p, num_vars);
-  EXPECT_EQ(CONEX_SUCCESS, status);
-
-  int id = 0;
-  status = CONEX_NewQuadraticCost(p, &id);
-
-  for (int i = 0; i < num_vars; i++) {
-    for (int j = 0; j < num_vars; j++) {
-      status = CONEX_UpdateQuadraticCostMatrix(p, id, i * num_vars + j, i, j);
-      EXPECT_EQ(CONEX_SUCCESS, status);
-    }
-  }
-
-  // Trigger out of bounds error.
-  status = CONEX_UpdateQuadraticCostMatrix(p, id, 1, num_vars, num_vars);
-  EXPECT_EQ(CONEX_FAILURE, status);
-
-  CONEX_DeleteConeProgram(p);
+  TEST_CONSTRAINT_API(CONEX_NewLinearInequality);
 }
---
+++ interfaces/test/interface_test_lmi.cc
@@ -0,0 +1,121 @@
+#include "interfaces/conex.h"
+
+#include "gtest/gtest.h"
+
+TEST(TestArguments, AddLMI) {
+  void* p = CONEX_CreateConeProgram();
+  int constraint_id = 0;
+  EXPECT_TRUE(CONEX_NewLinearMatrixInequality(p, 2, 2, &constraint_id) ==
+              CONEX_SUCCESS);
+  EXPECT_EQ(0, constraint_id);
+  EXPECT_TRUE(CONEX_NewLinearMatrixInequality(p, 2, 4, &constraint_id) ==
+              CONEX_SUCCESS);
+  EXPECT_EQ(1, constraint_id);
+
+  void* null_ptr = NULL;
+  EXPECT_TRUE(CONEX_NewLinearMatrixInequality(null_ptr, 2, 2, &constraint_id) ==
+              CONEX_FAILURE);
+
+  // TODO(FrankPermenter): Uncomment this test if it can be made more reliable.
+  // Currently, corrupted_ptr test is too weak and may not fail.
+  // void* corrupted_ptr = static_cast<double*>(p) + 1;
+  // EXPECT_TRUE(CONEX_NewLinearMatrixInequality(corrupted_ptr, 2,
+  // 2,&constraint_id) == CONEX_FAILURE);
+
+  int bad_complex_dim = 3;
+  EXPECT_TRUE(CONEX_NewLinearMatrixInequality(p, 2, bad_complex_dim,
+                                              &constraint_id) == CONEX_FAILURE);
+
+  int bad_order = 0;
+  EXPECT_TRUE(CONEX_NewLinearMatrixInequality(p, bad_order, 2,
+                                              &constraint_id) == CONEX_FAILURE);
+  CONEX_DeleteConeProgram(p);
+}
+
+TEST(TestArguments, UpdateLMI) {
+  void* p = CONEX_CreateConeProgram();
+  int num_vars = 4;
+  int status = CONEX_SetNumberOfVariables(p, num_vars);
+  EXPECT_EQ(CONEX_SUCCESS, status);
+
+  int constraint_id = 0;
+  int constraint_id_2 = 0;
+  int order = 2;
+  int hyper_complex_dim = 2;
+
+  status = CONEX_NewLinearMatrixInequality(p, order, hyper_complex_dim,
+                                           &constraint_id);
+  EXPECT_EQ(CONEX_SUCCESS, status);
+
+  status = CONEX_NewLinearMatrixInequality(p, order, hyper_complex_dim,
+                                           &constraint_id_2);
+  EXPECT_EQ(CONEX_SUCCESS, status);
+
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, order - 1,
+                                      order - 2, hyper_complex_dim - 1);
+  EXPECT_EQ(CONEX_SUCCESS, status);
+
+  int bad_hyper_complex_dim = hyper_complex_dim;
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, order - 1,
+                                      order - 2, bad_hyper_complex_dim);
+  EXPECT_EQ(CONEX_FAILURE, status);
+
+  int bad_order = order;
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, bad_order,
+                                      order - 2, hyper_complex_dim - 1);
+  EXPECT_EQ(CONEX_FAILURE, status);
+
+  int bad_index = order;
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, order - 1,
+                                      bad_index, hyper_complex_dim - 1);
+  EXPECT_EQ(CONEX_FAILURE, status);
+
+  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, order - 1, order - 2,
+                                  bad_hyper_complex_dim);
+  EXPECT_EQ(CONEX_FAILURE, status);
+
+  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, order - 1, order - 2,
+                                  hyper_complex_dim - 1);
+  EXPECT_EQ(CONEX_SUCCESS, status);
+
+  // Updating diagonal part of skew-symmetric matrix should fail.
+  status =
+      CONEX_UpdateAffineTerm(p, constraint_id, .3, 0, 0, hyper_complex_dim - 1);
+  EXPECT_EQ(CONEX_FAILURE, status);
+  CONEX_DeleteConeProgram(p);
+}
+
+TEST(TestArguments, SetVariables) {
+  int num_vars = 4;
+  void* p = CONEX_CreateConeProgram();
+
+  int status = CONEX_SetNumberOfVariables(p, num_vars);
+  EXPECT_EQ(CONEX_SUCCESS, status);
+
+  status = CONEX_SetNumberOfVariables(p, num_vars);
+  EXPECT_EQ(CONEX_FAILURE, status);
+  CONEX_DeleteConeProgram(p);
+}
+
+TEST(TestArguments, UpdateQuadraticCost) {
+  int num_vars = 4;
+  void* p = CONEX_CreateConeProgram();
+  int status = CONEX_SetNumberOfVariables(p, num_vars);
+  EXPECT_EQ(CONEX_SUCCESS, status);
+
+  int id = 0;
+  status = CONEX_NewQuadraticCost(p, &id);
+
+  for (int i = 0; i < num_vars; i++) {
+    for (int j = 0; j < num_vars; j++) {
+      status = CONEX_UpdateQuadraticCostMatrix(p, id, i * num_vars + j, i, j);
+      EXPECT_EQ(CONEX_SUCCESS, status);
+    }
+  }
+
+  // Trigger out of bounds error.
+  status = CONEX_UpdateQuadraticCostMatrix(p, id, 1, num_vars, num_vars);
+  EXPECT_EQ(CONEX_FAILURE, status);
+
+  CONEX_DeleteConeProgram(p);
+}
