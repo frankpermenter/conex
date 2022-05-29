f6e7c7506ce1dd1db08c953c9b1e9c5b30fce98f
Frank Permenter <frankpermenter@gmail.com>
2022-05-29 12:28:55 -0400

    Add getter and setter.

--- conex/equality_constraint.h
+++ conex/equality_constraint.h
@@ -24,9 +24,10 @@ class EqualityConstraints {
   EqualityConstraints(const Eigen::MatrixXd& A, const Eigen::MatrixXd& b);
 
   int SizeOfDualVariable() { return A_.rows(); }
+  Eigen::MatrixXd constraint_matrix() const { return A_; }
+  Eigen::MatrixXd affine_term() const { return b_; }
   Eigen::MatrixXd A_;
   Eigen::MatrixXd b_;
-  Eigen::VectorXd lambda_;
 
   friend int Rank(const EqualityConstraints&) { return 0; };
   friend void SetIdentity(EqualityConstraints*){};
