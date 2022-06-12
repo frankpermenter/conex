f1b25f2e11d89618fe9739cc5e1bef8d7fb0bc3a
Frank Permenter <frankpermenter@gmail.com>
2022-06-12 12:47:53 -0400

    Add storage for sqrt(w)

--- conex/workspace_soc.h
+++ conex/workspace_soc.h
@@ -7,9 +7,7 @@ namespace conex {
 struct WorkspaceSOC {
   WorkspaceSOC(int n) : n_(n), W(n + 1, 1) {}
 
-  static constexpr int size_of(int n) {
-    return get_size_aligned(n) + 4 * get_size_aligned(n);
-  }
+  static constexpr int size_of(int n) { return 2 + 5 * get_size_aligned(n); }
 
   friend int SizeOf(const WorkspaceSOC& o) { return size_of(o.n_); }
 
@@ -17,10 +15,12 @@ struct WorkspaceSOC {
     using Map = Eigen::Map<DenseMatrix, Eigen::Aligned>;
     int n = o->n_;
     new (&o->W1) Map(data, n, 1);
-    new (&o->temp1_1) Map(data + get_size_aligned(n), n, 1);
-    new (&o->temp2_1) Map(data + 2 * get_size_aligned(n), n, 1);
-    new (&o->temp3_1) Map(data + 3 * get_size_aligned(n), n, 1);
-    o->W0 = data + 4 * get_size_aligned(n);
+    new (&o->sqrt_W1) Map(data + get_size_aligned(n), n, 1);
+    new (&o->temp1_1) Map(data + 2 * get_size_aligned(n), n, 1);
+    new (&o->temp2_1) Map(data + 3 * get_size_aligned(n), n, 1);
+    new (&o->temp3_1) Map(data + 4 * get_size_aligned(n), n, 1);
+    o->W0 = data + 5 * get_size_aligned(n);
+    o->sqrt_W0 = data + 5 * get_size_aligned(n) + 1;
   }
 
   friend void print(const WorkspaceSOC& o) {
@@ -32,10 +32,12 @@ struct WorkspaceSOC {
   }
 
   double* W0 = nullptr;
+  double* sqrt_W0 = nullptr;
   double d0;
   double wsqrt_q1_norm_sqr;
   // TODO(FrankPermenter): Reduce number of temporaries.
   Eigen::Map<DenseMatrix, Eigen::Aligned> W1{NULL, 0, 0};
+  Eigen::Map<DenseMatrix, Eigen::Aligned> sqrt_W1{NULL, 0, 0};
   Eigen::Map<DenseMatrix, Eigen::Aligned> temp1_1{NULL, 0, 0};
   Eigen::Map<DenseMatrix, Eigen::Aligned> temp2_1{NULL, 0, 0};
   Eigen::Map<DenseMatrix, Eigen::Aligned> temp3_1{NULL, 0, 0};
