be204671cd195ba62f6d9ce98f0602c072eeab34
Frank Permenter <frankpermenter@gmail.com>
2026-06-03 13:38:13 -0400

    updates

--- doc/run.sh
+++ doc/run.sh
@@ -1,2 +1,7 @@
-latexmk integrator.tex
-latexmk yoshida_bregman_tutorial.tex
+#latexmk integrator.tex
+#latexmk yoshida_bregman_tutorial.tex
+#latexmk self_concordant_residual_levelset.tex
+#latexmk finite_distortion_residual_bound.tex 
+#latexmk sc_levelset_integral_theorem.tex 
+latexmk levelset_asymmetry_revised_sc_stability.tex
+
